3c74e35792846bcb5452df1661c1d13d26727f07
Saurav Kumar <saurav.kumar2@hm.com>
2024-11-15 12:13:51 +0530

    Init commit

--- MVVMCrossAndroidSample/Views/MainActivity.cs
+++ MVVMCrossAndroidSample/Views/MainActivity.cs
@@ -6,6 +6,7 @@ namespace MVVMCrossAndroidSample.Views
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public sealed class MainActivity : MvxActivity<MainViewModel>
     {
+        //Test Project for MVVMCross
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
