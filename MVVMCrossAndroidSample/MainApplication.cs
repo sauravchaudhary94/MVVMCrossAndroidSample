29e84e08128a4d2183477db48a3d0e568abbcc97
Saurav Kumar <saurav.kumar2@hm.com>
2024-11-15 12:10:30 +0530

    Add project files.

---
+++ MVVMCrossAndroidSample/MainApplication.cs
@@ -0,0 +1,19 @@
+﻿using Android.Runtime;
+using Core;
+using MvvmCross.Platforms.Android.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMCrossAndroidSample
+{
+    [Application]
+    public sealed class MainApplication : MvxAndroidApplication<Setup, App>
+    {
+        public MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+    }
+}
